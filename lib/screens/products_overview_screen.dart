a5988e018808aebbeb793a75397dcde87698e772
dario-nihil <dario.nihil@gmail.com>
2021-12-14 16:34:22 +0100

    added order item widget, orders screen and drawer

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -5,6 +5,7 @@ import '../screens/cart_screen.dart';
 import '../widgets/products_grid.dart';
 import '../widgets/badge.dart';
 import '../providers/cart.dart';
+import '../widgets/app_drawer.dart';
 
 enum FilterOptions {
   Favorites,
@@ -62,6 +63,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           )
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
