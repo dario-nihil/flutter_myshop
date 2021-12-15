260d6c8dd22e51016f0a48856817aaa709afabb2
dario-nihil <dario.nihil@gmail.com>
2021-12-15 09:08:30 +0100

    fix deprecation warning for snackbar

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -47,8 +47,8 @@ class ProductItem extends StatelessWidget {
             color: Theme.of(context).accentColor,
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
-              Scaffold.of(context).hideCurrentSnackBar();
-              Scaffold.of(context).showSnackBar(
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
                   content: Text(
                     'Item added to the cart',
