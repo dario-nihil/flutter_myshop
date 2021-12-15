669f225a50c7961154508dd5da7fb2527ac4e31c
dario-nihil <dario.nihil@gmail.com>
2021-12-15 10:23:24 +0100

    added user product screen and user product item widget

---
+++ lib/screens/user_product_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
+
+class UserProductScreen extends StatelessWidget {
+  static const routeName = '/user-product';
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView.builder(
+          itemBuilder: (_, idx) => Column(
+            children: [
+              UserProductItem(
+                productsData.items[idx].title,
+                productsData.items[idx].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+          itemCount: productsData.items.length,
+        ),
+      ),
+    );
+  }
+}
